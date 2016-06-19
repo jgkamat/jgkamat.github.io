8b2ba2c92f39fd63179ec09b97e6de096466ad38
Jay Kamat <jaygkamat@gmail.com>
2016-06-19 14:19:48 -0700

    Add function to autocalculate my stars!

--- src/project.el
+++ src/project.el
@@ -11,3 +11,32 @@
 <script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js\"></script>
 <script src=\"https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js\"></script>
 ")))
+
+(require 'request)
+(defun gh-stars (repo-string)
+  (progn (require 'request)
+    (defvar result nil)
+    (request (concat  "https://api.github.com/repos/" repo-string "/stargazers")
+      :parser 'json-read
+      :sync t
+      :success
+      (cl-function
+        (lambda (&key data &allow-other-keys)
+          (setq result (length data)))))
+    result))
+
+(defun parse-gh-str (url-string)
+  "Parses a github url and returns a nice representation like: jgkamat/alda-mode"
+  (let* ((var (split-string url-string "/"))
+          (len (length var)))
+    (when (string= (elt (last var) 0) "")
+      (setq len (- len 1)))
+    (concat (elt var (- len 2)) "/" (elt var (- len 1)))))
+
+(defun parse-org-link (org-link)
+  "takes in an org link and returns a string link"
+  (symbol-name (elt (elt org-link 0) 0)))
+
+(defun org-link-to-str (link)
+"Turns org links to jgkamat/alda-mode format"
+(parse-gh-str (parse-org-link link)))
