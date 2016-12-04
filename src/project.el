9259c6841ee2eab6dbc54841abd6cf3712dce9f1
Jay Kamat <jaygkamat@gmail.com>
2016-12-04 12:26:57 -0500

    Work on prev/next links

--- src/project.el
+++ src/project.el
@@ -23,38 +23,64 @@
 </div>")))
 
 
+
+;; Blog generators
 (defun org-timestamp-to-str (stamp)
   "Returns string value if org timestamp. Else, return stamp."
   (if (and (not (stringp stamp)) (eql (first stamp) 'timestamp))
     (plist-get (second stamp) ':raw-value)
     stamp))
 
-;; Blog generators
+(defun gen-org-property (filename)
+  "Generates an org property from a filename"
+  (let ((filename (file-truename filename)))
+    (with-temp-buffer
+      (insert-file-contents filename)
+      ;; TODO use a plist here instead of hacky ordering
+      `(,filename ,(plist-get (org-export-get-environment) ':date) ,(plist-get (org-export-get-environment) ':title)))))
+
+(defun gen-links-properties (&optional directory)
+  "Gens a sorted (by date) (filename . properties) from an org directory"
+  (let* ((directory (or directory (concat project-base "/blog")))
+          (files (directory-files directory t ".*\.org")))
+    (sort
+      ;; Map environments to (filename . property titles)
+      (mapcar #'gen-org-property files)
+      '(lambda (one two)
+         (let ((x (org-timestamp-to-str (first (second one))))
+                (y (org-timestamp-to-str (first (second two)))))
+           (when (or (eql 0 (org-2ft x)) (eql 0 (org-2ft y)))
+             (error (concat "Org parsing error found: "
+                      x " " y)))
+           (org-time< x y))))))
+
+(defun org-property-to-link (x)
+  "Turns a property genrated by gen-org-properties into an org link"
+  (format "[[file:%s][%s]]\n\n" (first x) (first (first (last x)))))
 ;; The directory you pass in must be the relative directory to work from (and must be relative to this file)
 (defun gen-links (&optional directory)
+  "Generates a list of links from a directory"
   (interactive)
-  (let* ((directory (or directory (concat project-base "/blog")))
-          (files (directory-files directory nil ".*\.org")))
-    ;; Reduce everything into a string
-    (reduce #'concat
-      ;; Map properties to strings
-      (mapcar '(lambda (x)
-                 (format "1. [[file:%s][%s]]\n\n" (first x) (first (first (last x)))))
-        (sort
-          ;; Map environments to (filename . property titles)
-          (mapcar '(lambda (x)
-                     (with-temp-buffer
-                       (let ((filename (concat directory "/" x)))
-                         (insert-file-contents filename)
-                         `(,x ,(plist-get (org-export-get-environment) ':date) ,(plist-get (org-export-get-environment) ':title)))))
-            files)
-          '(lambda (one two)
-             (let ((x (org-timestamp-to-str (first (second one))))
-                    (y (org-timestamp-to-str (first (second two)))))
-               (when (or (eql 0 (org-2ft x)) (eql 0 (org-2ft y)))
-                 (error (concat "Org parsing error found: "
-                          x " " y)))
-               (org-time< x y))))))))
+  ;; Reduce everything into a string
+  (reduce #'concat
+    ;; Map properties to strings
+    (mapcar '(lambda (x)
+               (format "1. %s" (org-property-to-link x)))
+      (gen-links-properties directory))))
+
+
+(defun gen-prev-next (&optional directory)
+  (interactive)
+  (let* ((current-property (gen-org-property buffer-file-name))
+         (properties (gen-links-properties directory))
+         (index (position properties current-property)))
+    (when (eql index nil)
+      (error "This org file was not part of this project"))
+    ;; TODO get next nad prev link
+    ))
+
+
+
 
 
 
